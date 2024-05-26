1cce2d0d4e6bab3d7e97903aa795979ae536c54d
MysticR <rahmanmahid25@gmail.com>
2024-05-26 22:16:17 +1000

    Quartz sync: May 26, 2024, 10:16 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🎲 Dungeons and Dragons 🐲",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
